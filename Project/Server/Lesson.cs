d1cc8ea75f71bfdb92afde6f6b0b6a3aa62b19e6
fkose3 <fkose3@gmail.com>
2013-09-16 13:30:55 +0300

    Dersler için bir sınıf oluşturduk

---
+++ Project/Server/Lesson.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server
+{
+    public class Lesson
+    {
+        private int lesson_id, teacher_id;
+        private string lesson_name, teacher_name;
+
+        public int LessonID
+        {
+            get { return lesson_id; }
+            set { lesson_id = value; }
+        }
+
+        public int TeacherID
+        {
+            get { return teacher_id; }
+            set { teacher_id = value; }
+        }
+
+        public string LessonName
+        {
+            get { return lesson_name; }
+            set { lesson_name = value; }
+        }
+
+        public string TeacherName
+        {
+            get { return teacher_name; }
+            set { teacher_name = value; }
+        }
+
+    }
+}
